6a882880925e5ff99c0ec2faffeb7d2279122ee0
Helene Pagniez <helenepagniez51@gmail.com>
2022-07-22 19:19:20 +0200

    header caché ok

--- frontend/groupomania/src/app/header.component/header.component.ts
+++ frontend/groupomania/src/app/header.component/header.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import { LoggedInUserId } from '../core/models/loggedInUserId.model';
+import { User } from '../core/models/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit{
+  user!: User;
+  loggedInUserId!: string | null;
+
   constructor(private router: Router) { }
+
+  ngOnInit() {
+    if (localStorage.getItem('loggedInUserId')===null) {
+      this.loggedInUserId = null;
+    }
+    else {
+      this.loggedInUserId = JSON.parse(localStorage.getItem('loggedInUserId') || '{}');
+    };
+  }
   
   @Input() title = 'groupomania';
 }
