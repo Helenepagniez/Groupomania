62fa2857cb1da322ef1d461e2dcb73b30770d629
Helene Pagniez <helenepagniez51@gmail.com>
2022-07-24 10:31:30 +0200

    modif profil avec et sans image côté back ok

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,9 +2,8 @@ const router= require('express').Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 const rateLimit = require('express-rate-limit'); // package de prévention des forces brutes
-const multer = require('multer');
+const multer = require('../middleware/multer-config');
 const {requireAuth} = require('../middleware/auth.middleware');
-const upload = multer();
 
 const passLimiter = rateLimit({
     windowMs: 2 * 60 * 1000, // Temps défini (en minutes) pour tester l'application
@@ -19,7 +18,7 @@ router.get('/logout', requireAuth, authController.logout);//se déconnecter
 //user display: 'block'
 router.get('/',requireAuth, userController.getAllUsers);//voir liste des utilisateurs
 router.get('/:id', requireAuth, userController.userInfo);// voir infos d'un utilisateur(profil)
-router.put('/:id', requireAuth, userController.updateUser);//modifier un utilisateur
+router.put('/:id', requireAuth, multer,userController.updateUser);//modifier un utilisateur
 router.delete('/:id', requireAuth, userController.deleteUser);//supprimer un utilisateur
 router.patch('/follow/:id', requireAuth, userController.follow);//suivre un utilisateur
 router.patch('/unfollow/:id', requireAuth, userController.unfollow);//ne plus suivre un utilisateur
