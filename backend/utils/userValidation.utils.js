fe11a66bcf5976aea4c0bac9409f2a436f7fa88e
Helene Pagniez <helenepagniez51@gmail.com>
2022-08-04 21:29:10 +0200

    essaie regex

---
+++ backend/utils/userValidation.utils.js
@@ -0,0 +1,42 @@
+const Joi = require('joi');
+
+const userValidation = (body) => {
+    const UserShema = Joi.object({
+        name: Joi.string()
+        .regex(/^[a-zA-ZÀ-ÿ -]{2,40}$/)
+        .lowercase()
+        .trim()
+        .required(),
+
+        firstname: Joi.string()
+        .regex(/^[a-zA-ZÀ-ÿ -]{2,40}$/)
+        .trim()
+        .required(),
+
+        email: Joi.string()
+        .lowercase()
+        .trim()
+        .email({ minDomainSegments: 2 })
+        .required(),
+
+        password: Joi.string()
+        .required()
+        .regex(/^[a-zA-ZÀ-ÿ -]{6,40}$/),
+
+        job: Joi.string()
+        .required(),
+
+        picture: Joi.string(),
+
+        bio: Joi.string(),
+
+        followers: Joi.string(),
+
+        following: Joi.string(),
+
+        likes: Joi.string(),
+    });
+    return UserShema.validate(body)
+};
+
+module.export = userValidation
